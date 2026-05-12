107c4c64cd534055f647064750fd8cee3f51dafa
willyko04 <willy.ko@cooper.edu>
2026-05-12 15:34:39 +0900

    live trader addition

--- README.md
+++ README.md
@@ -1,22 +1,24 @@
 # Crypto-momentum-backtester
 
-This is a project about analyzing the moving average of Bitcoin prices to find patterns and backtest a trading strategy. I used Binance's public API to get the data.
+This is a project about analyzing the moving average of Bitcoin prices to find patterns and backtest a trading strategy, and putting that to test in live date. I used Binance's public API to get the data.
 
 ### The Strategy
 The program compares the 30-hour and 10-hour moving averages of BTC closing prices:
 * **Buy:** When the 10-hour moving average is higher than the 30-hour moving average.
 * **Sell:** When the 10-hour moving average falls below the 30-hour moving average.
+In the live trader version, to see results more quickly, I changed the 10/30 hour moving average to 10/30 minutes moving average. 
 
 ### Performance
 After testing on 1,000 hours of data, the program saw an 18.2% gain. In comparison, simply buying and holding BTC over the same period would have only returned 13.6%. 
 
 ### Future Improvements
-I plan to add a stop-loss mechanism to protect against sudden price drops and experiment with different time intervals to see if the strategy holds up across longer periods.
+I plan to receive live data from binance using websockets to simulate trading in real time and see if my strategy is working real time.
 
 ### Installation
-You need to install pandas, numpy, and matplotlib via pip:
+You need to install pandas, numpy, and matplotlib for the backtester, and websockets for the live trader:
 ```bash
-pip install pandas numpy matplotlib
+pip install pandas numpy matplotlib websockets
 ```
 ### How to use
-To run the program, simply run the main.ipynb notebook in order. It will fetch the data and calculate the returns automatically.
+To run the backtester, simply run the backtester.ipynb notebook in order. It will fetch the data and calculate the returns automatically. 
+To test it with live date, run the live_trader.py. The program will receive live data through websocket from binance. Minimum of 30 minutes must be waited to observe its first buy/sell.
--- live_trader.py
+++ live_trader.py
@@ -6,34 +6,62 @@ from collections import deque
 d1 = deque(maxlen=10)
 d2 = deque(maxlen=30)
 
-async def hello():
-    url = "wss://stream.binance.com:9443/ws/btcusdt@kline_1m"
-    async with websockets.connect(url) as websocket:
-        
-        while(True):
-            btc_data = await websocket.recv()
-            data = json.loads(btc_data)
-            if(data["k"]["x"]):
-                print(data["k"]["c"])
-                print(data["k"]["v"])
-                print(data["k"]["x"])
-                d1.append(float(data["k"]["c"]))
-                d2.append(float(data["k"]["c"]))
-            if(len(d1)>=10):
-                ma10 = sum(d1) / 10 
-                print(ma10)
-            if(len(d2) >= 30):
-                ma30 = sum(d2) / 30 
-                print(ma30)
-                if(ma10<ma30):
-                    print(f"BUY at ${data['k']['c']}")
-                else:
-                    print(f"SELL at ${data['k']['c']}")
 
+async def btc_quant():
+    url = "wss://stream.binance.com:9443/ws/btcusdt@kline_1m"
+    start_price= None
+    bought = 0
+    sold = 0
+    while(True):
+        try:
+            async with websockets.connect(url) as websocket:
+                while(True):
+                    btc_data = await websocket.recv()
+                    data = json.loads(btc_data)
+
+                    #The data comes in a dictionary. The data we need is in the key 'k'
+                    # 'k' is another dictionary where 'x' key has the boolean data of if the price is closed or not.
+                    if(data["k"]["x"]):
+                        if(start_price is None):
+                            start_price = float(data["k"]["c"])
+                            print(f"Start Price! {start_price}")
+                        #'c' contains data on the closing price of the bitcoin
+                        print(data["k"]["c"])
+                        #'v' contains data on the volume of the bitcoin trades
+                        print(data["k"]["v"])
+                        print(data["k"]["x"])
+                        d1.append(float(data["k"]["c"]))
+                        d2.append(float(data["k"]["c"]))
+                        if(len(d1)>=10):
+                            ma10 = sum(d1) / 10 
+                            print(ma10)
+                        if(len(d2) >= 30):
+                            ma30 = sum(d2) / 30 
+                            print(ma30)
+                            current_price = float(data['k']['c'])
+                            if(ma10<ma30):
+                                print(f"BUY at ${data['k']['c']}")
+                                bought = data['k']['c']
+                            else:
+                                print(f"SELL at ${data['k']['c']}")
+                                buynhold = (current_price-start_price) / start_price * 100
+                                sold = (current_price-bought) / bought *100
+                                if(sold>0): print(f"Net profit of {sold} gained \n")
+                                else: print(f"Net loss of {sold} lost")
+                                print(f"While buy&hold performed {buynhold}")
+        
+
+                                
+                                
+                                
+        except websockets.ConnectionClosed:
+            print("Disconnected, reconnecting..")
+            await asyncio.sleep(3)
+            continue
 
             
             
 
 
 if __name__ == "__main__" :
-    asyncio.run(hello())
+    asyncio.run(btc_quant())
